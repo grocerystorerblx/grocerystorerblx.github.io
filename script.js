fb34a4920543594618e6fc6b41f0c64a651c9f85
TheDurbie <gavinwappler@gmail.com>
2023-10-16 18:24:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,14 +10,15 @@ var x = setInterval(function() {
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  // Time calculations for weeks, days, hours, minutes, and seconds
+  var weeks = Math.floor(distance / (1000 * 60 * 60 * 24 * 7));
+  var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var seconds = Math.floor((distance % (1000 * 60) / 1000));
 
-  // Display the result in the element with id="demo"
-  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  // Display the result in the element with id "countdown"
+  document.getElementById("countdown").innerHTML = weeks + "w " + days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
